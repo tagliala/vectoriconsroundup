462ca27743aadf0bb9964fcb4180e7f7b17498d8
Geremia Taglialatela <tagliala.dev@gmail.com>
2015-12-19 16:50:28 +0100

    Add Ionicons Close #2

--- scrape.rb
+++ scrape.rb
@@ -17,15 +17,19 @@ puts "Scraping Material Design Icons..."
 mdi_icons = icon_names 'https://raw.githubusercontent.com/Templarian/MaterialDesign-Webfont/master/css/materialdesignicons.css', /^\.mdi-(.+):before/
 puts "Done. Found #{mdi_icons.count} classes\n\n"
 
+puts "Scraping Ionicons..."
+ion_icons = icon_names 'https://raw.githubusercontent.com/driftyco/ionicons/master/scss/_ionicons-icons.scss', /^\.\#{\$ionicons-prefix}(.+):before/
+puts "Done. Found #{ion_icons.count} classes\n\n"
+
 puts "Scraping Elusive icons..."
 els_icons = icon_names 'https://raw.githubusercontent.com/reduxframework/elusive-icons/master/scss/_icons.scss', /^\.\#{\$el-css-prefix}-(.+):before/
 puts "Done. Found #{els_icons.count} classes\n\n"
 
-icons = (fa_icons + gl_icons + mdi_icons + els_icons).uniq.sort
+icons = (fa_icons + gl_icons + mdi_icons + ion_icons + els_icons).uniq.sort
 
 puts "Output table"
 for icon in icons
   puts <<-HTML
-        <tr><td><a href="##{icon}" id="#{icon}">#{icon}</a></td><td><i class="fa fa-#{icon}"></i></td><td><i class="glyphicon glyphicon-#{icon}"></i></td><td><i class="mdi mdi-#{icon}"></i></td><td><i class="el el-#{icon}"></i></td></tr>
+        <tr><td><a href="##{icon}" id="#{icon}">#{icon}</a></td><td><i class="fa fa-#{icon}"></i></td><td><i class="glyphicon glyphicon-#{icon}"></i></td><td><i class="ion-#{icon}"></i></td><td><i class="mdi mdi-#{icon}"></i></td><td><i class="el el-#{icon}"></i></td></tr>
   HTML
 end
